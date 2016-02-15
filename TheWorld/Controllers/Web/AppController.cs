99ce0d3800194242950efe3aa26366d3aff67f8c
Ashwini Laxminarayana <ashwini.pl@gmail.com>
2016-02-15 21:17:40 +0000

    original

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -7,14 +7,14 @@ namespace TheWorld.Controllers.Web
 {
   public class AppController : Controller
   {
-        private IMailService _mailService;
+    private IMailService _mailService;
 
-        public AppController(IMailService service)
-        {
-            _mailService = service;
-        }
+    public AppController(IMailService service)
+    {
+      _mailService = service;
+    }
 
-        public IActionResult Index()
+    public IActionResult Index()
     {
       return View();
     }
@@ -29,31 +29,16 @@ namespace TheWorld.Controllers.Web
       return View();
     }
 
-        [HttpPost]
-        public IActionResult Contact(ContactViewModel model)
-        {
+    [HttpPost]
+    public IActionResult Contact(ContactViewModel model)
+    {
             if (ModelState.IsValid)
             {
-                var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
 
-                if (string.IsNullOrWhiteSpace(email))
-                {
-                    ModelState.AddModelError("", "Could not send email, configuration problem.");
-                }
+                ViewBag.Message = "Mail Sent. Thanks!";
 
-                if (_mailService.SendMail(email,
-                  email,
-                  $"Contact Page from {model.Name} ({model.Email})",
-                  model.Message))
-                {
-                    ModelState.Clear();
-
-                    ViewBag.Message = "Mail Sent. Thanks!";
-
-                }
             }
-
-            return View();
-        }
+      return View();
     }
+  }
 }
