5afbc0b16211b53b4b70d852d3af83b993636d12
Ashwini Laxminarayana <ashwini.pl@gmail.com>
2016-02-21 16:50:57 +0000

    Added stopwatch and small changes

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using LibGit2Sharp;
 using System.Web.Script.Serialization;
 using System;
+using System.Diagnostics;
 
 namespace TheWorld.Controllers.Web
 {
@@ -38,23 +39,43 @@ namespace TheWorld.Controllers.Web
     [HttpPost]
     public IActionResult GetAQuoteInSQL(QuoteViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-
-                ViewBag.Message = "Mail Sent. Thanks!";
-
-            }
+            var watch = Stopwatch.StartNew();
+            // the code that you want to measure comes here
 
             var Quote = new Quote()
             {
                 FirstName = model.FirstName,
                 LastName = model.LastName,
                 Email = model.Email,
-                PostCode = model.PostCode
+                PostCode = model.PostCode,
+                Created = DateTime.Now
             };
+
+            string quoteNumber = RandomString(8);
+            Quote.QuoteNumber = quoteNumber;
             _context.Quotes.Add(Quote);
             _context.SaveChanges();
+            var firstCompany = (from c in _context.Quotes select c).FirstOrDefault();
+            var kvp = firstCompany.Id;
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            if (ModelState.IsValid)
+            {
+
+                ViewBag.Message = "Thanks! Your Quote is saved. Your quote number is: " + quoteNumber + "\\n" + "Time taken: " + elapsedMs + "ms";
+
+            }
             return View();
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
         public IActionResult GetAQuoteViaGit()
@@ -66,12 +87,8 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult GetAQuoteViaGit(QuoteViewModel model)
         {
-            if (ModelState.IsValid)
-            {
+            var watch = Stopwatch.StartNew();
 
-                ViewBag.Message = "Mail Sent. Thanks!";
-
-            }
             string rootedPath = Repository.Init("C:\\temp\\rooted\\path");
             string jsondata = new JavaScriptSerializer().Serialize(model);
               
@@ -86,11 +103,29 @@ namespace TheWorld.Controllers.Web
                 repo.Stage("output.json");
 
                 // Create the committer's signature and commit
-                Signature author = new Signature("James", "@jugglingnutcase", DateTime.Now);
+                Signature author = new Signature(model.FirstName, model.Email, DateTime.Now);
                 Signature committer = author;
 
                 // Commit to the repository
-                Commit commit = repo.Commit("Here's a commit i made!", author, committer);
+                try {
+                    Commit commit = repo.Commit("Quote committed", author, committer);
+                    watch.Stop();
+                    var elapsedMs = watch.ElapsedMilliseconds;
+
+                    if (ModelState.IsValid)
+                    {
+
+                        ViewBag.Message = "Thanks! Your Quote is saved. Your quote number is: " + commit.Id + "\\n" + "Time taken: " + elapsedMs + "ms";
+
+                    }
+
+                }
+                catch(Exception ex)
+                {
+                    ViewBag.Message = "Nothing to commit";
+                }
+                    
+
             }
 
             return View();
